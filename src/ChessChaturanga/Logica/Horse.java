5395ed653ec66c902b13f4f98e317e78da826f86
KenyStev <kjestevez.0795@gmail.com>
2015-02-19 18:04:07 -0600

    fix de Horse

--- src/ChessChaturanga/Logica/Horse.java
+++ src/ChessChaturanga/Logica/Horse.java
@@ -5,82 +5,38 @@
  */
 package ChessChaturanga.Logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dmenjivar
  */
 public class Horse extends Piece{
 
-    public Horse(String name, Color color, int row, int col) {
-        super("Horse", color, row, col);
+    public Horse(Color color, int row, int col) {
+        super("Caballo", color, row, col);
     }
 
     @Override
     protected Piece genereMovementsValid(Board b) {
-        int row = position.row, col = position.col;
-        Piece p;
-        if(row<b.SIZE-1){
-            p = b.getPieceAt(row+1, col);
-            if((p==null) || (p!=(null) && isEnemy(p)))
-                movementsValids.add(new Position(row+1, col));
-            if(col>0){
-                p = b.getPieceAt(row+1, col-1);
-                if((p==null) || (p!=(null) && isEnemy(p)))
-                    movementsValids.add(new Position(row+1, col-1));
-            }
-            if(col<b.SIZE-1){
-                p = b.getPieceAt(row+1, col+1);
-                if((p==null) || (p!=(null) && isEnemy(p)))
-                    movementsValids.add(new Position(row+1, col+1));
-            }
-        }
-        
-        if(row>0){
-            p = b.getPieceAt(row-1, col);
-            if((p==null) || (p!=(null) && isEnemy(p)))
-                movementsValids.add(new Position(row-1, col));
-            if(col>0){
-                p = b.getPieceAt(row-1, col-1);
-                if((p==null) || (p!=(null) && isEnemy(p)))
-                    movementsValids.add(new Position(row-1, col-1));
-            }
-            if(col<b.SIZE-1){
-                p = b.getPieceAt(row-1, col+1);
-                if((p==null) || (p!=(null) && isEnemy(p)))
-                    movementsValids.add(new Position(row-1, col+1));
-            }
-        }
-        
-        if(col>0){
-            p = b.getPieceAt(row, col-1);
-            if((p==null) || (p!=(null) && isEnemy(p)))
-                movementsValids.add(new Position(row, col-1));
-        }
-        if(col<b.SIZE-1){
-            p = b.getPieceAt(row, col+1);
-            if((p==null) || (p!=(null) && isEnemy(p)))
-                movementsValids.add(new Position(row, col+1));
-        }
-        return this;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     protected boolean validMovement(int row, int col) {
-        for (Position m : movementsValids) {
-            if(m.validar(row, col))
-                return true;
-        }
-        return false;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    protected boolean mover(Board b, int row, int col) {
-        boolean isvalid = validMovement(row, col);
-        if(isvalid){
-            position.set(row, col);
-            genereMovementsValid(b);
-        }
-        return isvalid;
+    public boolean mover(Board b, int row, int col) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public ArrayList<Position> getMovementsValids(Board b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
     
 }
